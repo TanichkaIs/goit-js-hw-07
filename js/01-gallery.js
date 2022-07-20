5af7c5dbea4cf16b1bf2ba7fb13fc05d7a19e83d
TanichkaIs <93533516+TanichkaIs@users.noreply.github.com>
2022-07-20 22:57:54 +0200

    20072022

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const paletteContainer = document.querySelector('.gallery');
+
+function createPhotoCardMarkup (galleryItems) {
+    return galleryItems.map(({ preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    }
+    ).join('');
+}
+
+const cardsMarkup = createPhotoCardMarkup(galleryItems);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+paletteContainer.addEventListener('click', openImageClick);
+
+function openImageClick(e) {
+    e.preventDefault();
+
+    const modal = basicLightbox.create(
+        `
+        <img src="${e.target.dataset.source}" width="800", height="600"> `,
+        {
+            onShow: () => window.addEventListener('keydown', onPressKeyESC),
+            onClose: () => window.removeEventListener('keydown', onPressKeyESC),
+        }
+    );
+    modal.show();
+
+
+function onPressKeyESC(e) {
+    if (e.code === 'Escape') {
+        modal.close();
+    }
+}
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+const paletteContainer = document.querySelector('.gallery');
+
+function createPhotoCardMarkup (galleryItems) {
+    return galleryItems.map(({ preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    }
+    ).join('');
+}
+
+const cardsMarkup = createPhotoCardMarkup(galleryItems);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: '250'
+    
+})
+
+
 console.log(galleryItems);
